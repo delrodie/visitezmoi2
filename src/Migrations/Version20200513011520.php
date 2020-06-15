6aae77bb18aed081676e06ba52b7dc66149602ca
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-06-15 12:18:11 +0000

    MAJ Migrations

--- src/Migrations/Version20200513011520.php
+++ src/Migrations/Version20200513011520.php
@@ -23,7 +23,6 @@ final class Version20200513011520 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, nombre_bien INT DEFAULT NULL, slug VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_97CA47AB989D9B62 ON mode (slug)');
     }
 
     public function down(Schema $schema) : void
@@ -32,6 +31,5 @@ final class Version20200513011520 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE categorie');
-        $this->addSql('DROP INDEX UNIQ_97CA47AB989D9B62 ON mode');
     }
 }
